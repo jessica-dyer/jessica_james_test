d7660b332ab592798c7452c69cc2d81cfdf3cf6c
jessica-dyer <jessica.dyer@gmail.com>
2021-02-10 14:10:47 -0800

    Initial commit.

---
+++ hello_world.R
@@ -0,0 +1,10 @@
+##################################################
+## Project: James practicing with GitHub
+## Script purpose: 
+## Date: 2021-02-10
+## Author: Jessica Dyer
+##################################################
+
+## Hi James! This is our first R project with a Git Repo! Exciting! Here is 
+## your task: Please make some changes to this document by adding some text 
+## to this document, saving it, and pushing your changes to the repo. 
