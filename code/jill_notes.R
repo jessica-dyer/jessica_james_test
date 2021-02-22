a4eea78b633909de6bde95c79db378938df4e1fa
jessica-dyer <jessica.dyer@gmail.com>
2021-02-22 08:24:22 -0800

    Created a jill_notes file with a question in there.

---
+++ code/jill_notes.R
@@ -0,0 +1,11 @@
+##################################################
+## Project: Test repo for James and Jill
+## Script purpose: Get Jill practice with pushing a pulling!
+## Date: 2021-02-22
+## Author: Jessica Dyer
+##################################################
+
+# Suppose I have a vector x <- c(3, 5, 1, 10, 12, 6) and I want to set all 
+# elements of this vector that are less than 6 to be equal to zero. What R code achieves this? 
+
+x <- c(3, 5, 1, 10, 12, 6)
